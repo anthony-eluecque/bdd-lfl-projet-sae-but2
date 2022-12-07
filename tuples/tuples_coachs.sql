4a3151df60999323e5bb87907fe6ce07c8d43652
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-07 17:33:03 +0100

    Add commentary

--- tuples/tuples_coachs.sql
+++ tuples/tuples_coachs.sql
@@ -14,13 +14,23 @@
 -- TUPLES POUR LA TABLE COACHS DE LA DATABASE LEAGUE OF LEGENDS LFL 2022 --
 
 INSERT INTO Coachs(pseudo_coach, nom_coach, prenom_coach,  id_nationalite) VALUES
+-- LDLC OL
 ('Zeph', 'VIGUIE', 'Quentin', 64),
+-- BDS ACADEMY
 ('GoToOne', 'PICARD', 'Adrien',  64),
+-- KARMINE CORP
 ('Striker', 'KELLA', 'Yanis', 64),
+-- VITALITY.BEE
 ('Realistik', 'RUSE', 'Andrei', 148),
+-- MISFITS PREMIER
 ('Delord', 'SZABLA', 'Pawel', 144),
+-- TEAM GO
 ('Jesiz', 'LE', 'Jesse', 48),
+-- SOLARY
 ('Jon', 'ELLIS', 'Jonathan', 25),
+-- GAMEWARD
 ('Malau', 'RESTOUBLE', 'Malaury', 64),
+-- MIRAGE ELYANDRA
 ('Hellombre', 'L', 'Adrien', 64),
+-- TEAM OPLON
 ('Aries', 'BIGANZOLI', 'Grégoire', 64);
