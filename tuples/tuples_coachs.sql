2e9d360f290b19b0ba4de584793669dc9f1b3a17
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-07 15:21:29 +0100

    Tuples pour la table coach (updated)

---
+++ tuples/tuples_coachs.sql
@@ -0,0 +1,23 @@
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■       ■■■■■■■            ■■■■       ■■■■■■■■■■ --
+-- ■■■■■■  ■■   ■■■■■■■   ■■■■■■■  ■■■■  ■■   ■■■■■■■■■■ --
+-- ■■■■■■  ■■   ■■■■■■■   ■■       ■■■■  ■■   ■■■■■■■■■■ --
+-- ■■■■■■  ■■      ■■■■   ■■■■   ■■■■■■  ■■       ■■■■■■ --
+-- ■■■■■■  ■■■■■   ■■■■   ■■     ■■■■■■  ■■■■■■   ■■■■■■ --
+-- ■■■■■■  ■■■■■   ■■■■   ■■   ■■■■■■■■  ■■■■■■   ■■■■■■ --
+-- ■■■■■■          ■■■■        ■■■■■■■■           ■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- Auteur : Eluecque Anthony, Fournier Benjamin, Dournel Frédéric
+
+-- TUPLES POUR LA TABLE COACHS DE LA DATABASE LEAGUE OF LEGENDS LFL 2022 --
+
+INSERT INTO Coachs(pseudo_coach, nom_coach, prenom_coach,  id_nationalite) VALUES
+('Zeph', 'VIGUIE', 'Quentin', 67),
+('GoToOne', 'PICARD', 'Adrien',  67),
+('Striker', 'KELLA', 'Yanis', 67),
+('Realistik', 'RUSE', 'Andrei', 148),
+('Delord', 'SZABLA', 'Pawel', 144),
+('Jesiz', 'LE', 'Jesse', 48),
+('Jon', 'ELLIS', 'Jonathan', 25),
