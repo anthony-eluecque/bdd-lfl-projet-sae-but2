66e03f51df137185381d639fc44a11eac44e3c94
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-07 16:26:26 +0100

    Joueurs table insert

---
+++ tuples/tuples_joueurs.sql
@@ -0,0 +1,71 @@
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■       ■■■■■■■            ■■■■       ■■■■■■■■■■ --
+-- ■■■■■■  ■■   ■■■■■■■   ■■■■■■■  ■■■■  ■■   ■■■■■■■■■■ --
+-- ■■■■■■  ■■   ■■■■■■■   ■■       ■■■■  ■■   ■■■■■■■■■■ --
+-- ■■■■■■  ■■      ■■■■   ■■■■   ■■■■■■  ■■       ■■■■■■ --
+-- ■■■■■■  ■■■■■   ■■■■   ■■     ■■■■■■  ■■■■■■   ■■■■■■ --
+-- ■■■■■■  ■■■■■   ■■■■   ■■   ■■■■■■■■  ■■■■■■   ■■■■■■ --
+-- ■■■■■■          ■■■■        ■■■■■■■■           ■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ --
+-- Auteur : Eluecque Anthony, Fournier Benjamin, Dournel Frédéric
+
+-- TUPLES POUR LA TABLE JOUEURS DE LA DATABASE LEAGUE OF LEGENDS LFL 2022 --
+
+
+INSERT INTO Joueurs(pseudo, nom, prenom , date_naissance , id_nationalite) VALUES 
+-- LDLC OL
+('Ragner', 'ASLAN', 'Onurcan', '19-12-2000', 184),
+('Yike', 'SUNDELIN', 'Martin', '11-11-2000' ,173),
+('Eika', 'VALDENAIRE', 'Jérémy', '19-07-1996' ,64),
+('Exakick', 'FOUCOU', 'Thomas', '28-09-2003' ,64),
+('Doss', 'SCHWARTZ', 'Mads', '19-03-1999' ,48),
+-- BDS ACADEMY
+('Adam', 'MAANANE', 'Adam', '30-12-2001',64),
+('Sheo', 'BORILE', 'Théo', '05-07-2001' ,64),
+('Reeker', 'CHEN', 'Steven', '15-05-2001' ,68),
+('Crownie', 'MARUSIC', 'Jus','17-04-1998' ,163),
+('Erdote', 'NOWAK', 'Robert', '19-07-1999' ,144),
+-- KARMINE CORP
+('Cabochard', 'SIMON-MESLET', 'Lucas','15-04-1997' , 64),
+('113', 'BALCI', 'Dokugan','12-08-2004' , 184),
+('Saken', 'FAYARD', 'Lucas','05-11-1998' , 64),
+('Rekkles', 'LARSSON', 'Carl Martin Erik','20-09-1996' , 173),
+('Hantera', 'BOURGEOIS', 'Jules','21-02-1999' , 64), 
+-- VITALITY.BEE
+('Szygenda', 'JENSEN', 'Mathias', '14-04-2001', 48),
+('Skeanz', 'MARQUET', 'Duncan', '25-09-2000', 64),
+('Diplex', 'PONOMAREV', 'Dimitri', '15-07-2003', 68),
+('Jeskla', 'KLARIN STROMBERG', 'Jesper', '08-07-2000', 173),
+('Jactroll', 'SKURZYNSKI', 'Jakub', '05-08-1998', 144),
+-- MISFITS PREMIER
+('Kackos', 'KUBZIAKOWSKI', 'Krzysztof', NULL, 144),
+('Shlatan', 'AHMAD', 'Lucjan', '23-07-2001', 144),
+('Czajek', 'CZAJKA', 'Mateusz', '24-09-2003', 144),
+('Woolite', 'PRUSKI', 'Pawet', '07-05-1996', 144),
+('Vander', 'BOGDAN', 'Oskar', '18-04-1994', 144),
+-- TEAM GO
+('Nuq', 'ERDOGDU', 'Erkmen', '12-05-2003', 184),
+('Karim kt', 'AUBINEAU', 'Karim', '19-03-1997', 64),
+('Ronaldo', 'BETEA', 'Ronaldo', '23-12-1998', 148),
+('Smiley', 'GRANQUIST', 'Ludvig Erik Hugo', '30-06-1998', 173),
+('Veignorem', 'LUSSIEZ', 'Dai-vinh', '31-01-2000', 64),
+-- SOLARY
+('Kio', 'KRALIK', 'Simon', '16-07-2004',162),
+('Djoko', 'GUILLARD', 'Charly','01-03-1997',64),
+('Scarlet', 'WIEDERHOFER', 'Marcel','18-11-1998', 11),
+('Asza', 'JACOBS', 'Patrick',NULL, 129),
+('Steeelback', 'MEDJALDI', 'Pierre','16-08-1996', 64),
+-- GAMEWARD
+('Melonik', 'SLECZKA', 'Dawid', '26-04-2002', 144),
+('Akabane', 'LE', 'Philippe', '15-10-2000', 64),
+('Czekolad', 'SZCZEPANIK', 'Pawel', '15-02-2003', 144),
+('Innaxe', 'ALIEV', 'Nihat', '23-03-1999', 27),
+('Kamilius', 'KOST')
+-- MIRAGE ELYANDRA
+('Badlulu', 'PIOCHAUD', 'Lucas','',)
+('Memento','','','',)
+('RangJun','','','',)
+('CodySun','','','')
+('Raxxo')
