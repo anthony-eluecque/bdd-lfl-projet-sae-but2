f7e449571d8d955a42450f0d9230f5c5684be917
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-07 15:22:01 +0100

    Edit Coach Table

--- tuples/tuples_coachs.sql
+++ tuples/tuples_coachs.sql
@@ -14,10 +14,13 @@
 -- TUPLES POUR LA TABLE COACHS DE LA DATABASE LEAGUE OF LEGENDS LFL 2022 --
 
 INSERT INTO Coachs(pseudo_coach, nom_coach, prenom_coach,  id_nationalite) VALUES
-('Zeph', 'VIGUIE', 'Quentin', 67),
-('GoToOne', 'PICARD', 'Adrien',  67),
-('Striker', 'KELLA', 'Yanis', 67),
+('Zeph', 'VIGUIE', 'Quentin', 64),
+('GoToOne', 'PICARD', 'Adrien',  64),
+('Striker', 'KELLA', 'Yanis', 64),
 ('Realistik', 'RUSE', 'Andrei', 148),
 ('Delord', 'SZABLA', 'Pawel', 144),
 ('Jesiz', 'LE', 'Jesse', 48),
 ('Jon', 'ELLIS', 'Jonathan', 25),
+('Malau', 'RESTOUBLE', 'Malaury', 64),
+('Hellombre', 'L', 'Adrien', 64),
+('Aries', 'BIGANZOLI', 'Grégoire', 64);
