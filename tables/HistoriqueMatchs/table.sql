5af8b9d6063a8d9e6aa1466d317a4fb85ce526d7
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-08 20:11:34 +0100

    Fix Table

--- tables/HistoriqueMatchs/table.sql
+++ tables/HistoriqueMatchs/table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Historique_Matchs(
     id_historique_match SERIAL PRIMARY KEY,
-    id_match INTEGER NOT NULL REFERENCES Joueurs(id_joueur),
-    id_joueur INTEGER NOT NULL REFERENCES Matchs(id_match),
+    id_match INTEGER NOT NULL REFERENCES Matchs(id_match),
+    id_joueur INTEGER NOT NULL REFERENCES Joueurs(id_joueur),
     id_champion_choisi INTEGER NOT NULL REFERENCES Champions(id_champion),
     id_champion_banni INTEGER NOT NULL REFERENCES Champions(id_champion),
     kills_joueur INTEGER NOT NULL,
