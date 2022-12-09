8a642aa600748e78be4632e150c4f97ab359a657
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-09 13:49:40 +0100

    Tables

---
+++ script/Tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE classement_LFL(
+    id_equipe INTEGER PRIMARY KEY,
+    nb_win INTEGER NOT NULL,
+    nb_lose INTEGER NOT NULL
+);
+
+CREATE TABLE statistique_LFL(
+    id_equipe INTEGER PRIMARY KEY,
+    winrate FLOAT ,
+    kda_equipe FLOAT,
+    moyenne_duree_game TIME
+);
