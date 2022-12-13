567668e25a0c0824e60138b6a95bbedf3166e12d
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-13 14:08:11 +0100

    Add week classement trigger

--- script/GestionClassement.sql
+++ script/GestionClassement.sql
@@ -1,15 +1,25 @@
+CREATE TABLE classement_week_lfl(
+    id_equipe INTEGER,
+    nb_win INTEGER,
+    nb_lose INTEGER,
+    week INTEGER
+);
+
 CREATE OR REPLACE FUNCTION gestion_classement() RETURNS TRIGGER AS $$
 DECLARE
     v_id_equipe_gagnante classement_LFL.id_equipe%type;
     v_id_equipe_perdante classement_LFL.id_equipe%type;
 
-    nb_win_existantes classement_LFL.nb_lose%type;
-    nb_loses_existantes classement_LFL.nb_win%type;
+    nb_win_existantes classement_LFL.nb_win%type;
+    nb_loses_existantes classement_LFL.nb_lose%type;
 
+    nb_week_win INTEGER;
+    nb_week_lose INTEGER;
 BEGIN
-
     nb_win_existantes :=0;
     nb_loses_existantes:=0;
+    nb_week_lose :=0;
+    nb_week_win := 0;
 
     SELECT id_equipe INTO v_id_equipe_gagnante FROM classement_LFL WHERE id_equipe = new.vainqueur;
     SELECT id_equipe INTO v_id_equipe_perdante FROM classement_LFL WHERE id_equipe = new.perdant;
@@ -23,7 +33,6 @@ BEGIN
         UPDATE classement_LFL SET nb_win = nb_win_existantes WHERE id_equipe = v_id_equipe_gagnante;
     END IF;
 
-
     IF (v_id_equipe_perdante IS NULL) THEN 
         INSERT INTO classement_LFL values(new.perdant,0,1); -- Si il n'existe pas encore alors il a que  le match qu'il vient de jouer
     ELSE  -- On s'occupe de modifier l'existant sinon  
@@ -34,6 +43,26 @@ BEGIN
     END IF;
 
 
+    SELECT id_equipe INTO v_id_equipe_gagnante FROM classement_week_lfl WHERE id_equipe = new.vainqueur AND week = new.num_semaine;
+    -- raise notice '% , %' , v_id_equipe_gagnante,v_id_equipe_perdante;
+    IF (v_id_equipe_gagnante IS NULL) THEN
+        INSERT INTO classement_week_lfl values (new.vainqueur,1,0,new.num_semaine);
+    ELSE    
+        SELECT nb_win INTO nb_week_win FROM classement_week_lfl WHERE id_equipe = v_id_equipe_gagnante AND week = new.num_semaine;
+        nb_week_win = nb_week_win + 1;
+        UPDATE classement_week_lfl SET nb_win = nb_week_win WHERE id_equipe = v_id_equipe_gagnante AND week = new.num_semaine;
+    END IF;
+
+    SELECT id_equipe INTO v_id_equipe_perdante FROM classement_week_lfl WHERE id_equipe = new.perdant AND week = new.num_semaine;
+    
+    IF (v_id_equipe_perdante IS NULL) THEN
+        INSERT INTO classement_week_lfl values (new.perdant,0,1,new.num_semaine);
+    ELSE 
+        SELECT nb_lose INTO nb_week_lose FROM classement_week_lfl WHERE id_equipe = v_id_equipe_perdante AND week = new.num_semaine;
+        nb_week_lose = nb_week_lose + 1;
+        -- raise notice 'nb_lose : %',nb_week_lose;
+        UPDATE classement_week_lfl SET nb_lose = nb_week_lose WHERE id_equipe = v_id_equipe_perdante AND week = new.num_semaine;
+    END IF;
 
     RETURN NEW;
 END;
