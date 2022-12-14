a5995fe891cc6c6cd4dc2f845f6bfb12b03608a0
Antorakk <95444282+Antorakk@users.noreply.github.com>
2022-12-14 23:38:59 +0100

    Meilleurs games features

--- script/MeilleursParties.sql
+++ script/MeilleursParties.sql
@@ -1,21 +1,23 @@
-create or replace function get_matchs_joueur(
-    vid_joueur Joueurs.id_joueur%type
-) RETURNS TABLE(
-    vid_match INTEGER,
-    kda DECIMAL
-) AS $$
-DECLARE
-    var_r RECORD;
-BEGIN 
-    FOR var_r IN (select id_match from historique_matchs
-                    WHERE id_joueur=vid_joueur)
-    LOOP    
-        vid_match = var_r.id_match;
-        kda = calcul_kda_joueur_match(vid_joueur,var_r.id_match);
-        return NEXT;
-    END LOOP;
-END;
-$$ language plpgsql;
+    create or replace function get_matchs_joueur(
+        vid_joueur Joueurs.id_joueur%type
+    ) RETURNS TABLE(
+        vid_match INTEGER,
+        kda DECIMAL,
+        id_champion INTEGER
+    ) AS $$
+    DECLARE
+        var_r RECORD;
+    BEGIN 
+        FOR var_r IN (select id_match,id_champion_choisi from historique_matchs
+                        WHERE id_joueur=vid_joueur)
+        LOOP    
+            vid_match = var_r.id_match;
+            kda = calcul_kda_joueur_match(vid_joueur,var_r.id_match);
+            id_champion = var_r.id_champion_choisi;
+            return NEXT;
+        END LOOP;
+    END;
+    $$ language plpgsql;
 
 
 create or replace function meilleurs_matchs_joueur(
@@ -29,9 +31,29 @@ BEGIN
     FOR var_r IN (
         SELECT vid_match FROM  get_matchs_joueur(v_id_joueur)
         ORDER BY kda LIMIT 3
+        -- Je crois avoir oublié le desc
     )
     LOOP
         v_id_match = var_r.vid_match;
+        return NEXT;
+    END LOOP;
+END;
+$$ language plpgsql;
+
+create or replace function get_champions_joueur(
+    v_id_joueur Joueurs.id_joueur%type
+) RETURNS TABLE(
+    champ_nom VARCHAR(50)
+) AS $$
+DECLARE
+    var_r RECORD;
+BEGIN
+    FOR var_r IN (
+        SELECT id_champion FROM get_matchs_joueur(v_id_joueur)
+        ORDER BY kda LIMIT 3
+    )
+    LOOP
+        champ_nom = getNomChampion(var_r.id_champion);
         return NEXT;
     END LOOP;
 END;
